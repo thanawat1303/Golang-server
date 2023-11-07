6af91046ac782cb7210f9514cd0bf8fbf225cf4c
thanawat1303 <fquarius13@gmail.com>
2023-11-07 22:32:05 +0700

    set encrpt AES md5 sha Base64 Hmac

--- Authentication/Crypt.go
+++ Authentication/Crypt.go
@@ -2,40 +2,93 @@ package main
 
 import (
 	"crypto/aes"
-	"crypto/cipher"
 	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
+	"hash"
 )
 
 func EncryptBase64(textEncryt string) string {
 	return base64.StdEncoding.EncodeToString([]byte(textEncryt))
 }
 
-func EncryptSha256(textEncryt string) string {
-	hash := sha256.New()           //create sha256
-	hash.Write([]byte(textEncryt)) //encrpt 256
-	hashed := hash.Sum(nil)        //input
+func EncryptMd5(textEncryt string) string {
+	hash := md5.New()
+	hash.Write([]byte(textEncryt))
+	hashed := hash.Sum(nil)
 
-	return
+	return hex.EncodeToString(hashed)
+}
+
+func EncryptSha1(textEncryt string) string {
+	hash := sha1.New()             //create sha256
+	hash.Write([]byte(textEncryt)) //write encrpt sha256 to hash
+	hashed := hash.Sum(nil)        //encrption sha256
+
+	return hex.EncodeToString(hashed)
+}
+
+func EncryptSha256(textEncryt string) string {
+	hash := sha256.New()
+	hash.Write([]byte(textEncryt))
+	hashed := hash.Sum(nil)
+
+	return hex.EncodeToString(hashed)
+}
+
+func EncryptSha512(textEncryt string) string {
+	hash := sha512.New()
+	hash.Write([]byte(textEncryt))
+	hashed := hash.Sum(nil)
+
+	return hex.EncodeToString(hashed)
 }
 
 func EncrytAES(key []byte, textEncryt string) string {
 	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err.Error()
+	}
 
 	out := make([]byte, len(textEncryt))
 
 	block.Encrypt(out, []byte(textEncryt))
-	print(out)
 
-	return En
+	return hex.EncodeToString(out)
+}
+
+func EncryptHmac(algorithm string, key []byte, textEncryt string) string {
+	var algorithmIn func() hash.Hash
+
+	switch algorithm {
+	case "sha256":
+		algorithmIn = sha256.New
+	case "sha512":
+		algorithmIn = sha512.New
+	case "sha1":
+		algorithmIn = sha1.New
+	case "md5":
+		algorithmIn = md5.New
+	default:
+		return "Inv algorithm"
+	}
+
+	encrpt := hmac.New(algorithmIn, key)
+	encrpt.Write([]byte(textEncryt))
+
+	return hex.EncodeToString(encrpt.Sum(nil))
 }
 
 func main() {
-	aes.NewCipher()
-	cipher.NewGCM()
-	hmac.New()
-	base64.NewEncoding()
-	fmt.Print()
+	fmt.Println(EncrytAES([]byte("thanawat1303"), "Hello"))
+	// aes.NewCipher()
+	// cipher.NewGCM()
+	// hmac.New()
+	// base64.NewEncoding()
+	// fmt.Print()
 }
